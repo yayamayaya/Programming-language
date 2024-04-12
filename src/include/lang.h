0c4273b69e70263bd3017e830013ca479beeedef
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-13 02:05:56 +0300

    добавление некоторых дефайнов

--- src/include/lang.h
+++ src/include/lang.h
@@ -2,5 +2,21 @@
 #define PROG_LANG
 
 #include "../lang_parser/tokenizator.h"
+#include "../../graph_creator/create_graph.h"
+
+#ifdef DEBUG
+#define TOK_SHIFT()\
+    tkns++;\
+    LOG("> tokens shifted, current token is:\n");\
+    token_dump(logs, tkns, 1)
+#else
+#define TOK_SHIFT() tkns++
+#endif
+
+#define _ADD_B(arg1, arg2)\
+    if (add_branch(arg1, arg2))\
+        return NULL
+
+node_t *create_syntax_tree(token_t *token_arr);    
 
 #endif
