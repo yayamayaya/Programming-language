c6ed1a5d05531045408dd6e23fb45f5c547d50ff
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-13 02:05:24 +0300

    добавлен вызов функции парсинга

--- src/main.cpp
+++ src/main.cpp
@@ -6,16 +6,23 @@ int main(int argc, char const *argv[])
 {
     assert(argc > 1);
     assert(argv[argc - 1]);
-    _OPEN_LOG("logs/token_test.log");
+    _OPEN_LOG("logs/main.log");
 
     token_t *token_arr = {0};
     
     int error = tokenize(&token_arr, argv[argc - 1]);
     if (error)
+    {
+        _CLOSE_LOG();
         return error;
+    }
     
+    node_t *root = create_syntax_tree(token_arr);
+    create_gparh_code(root, EXPR);
 
-    _CLOSE_LOG();
+    kill_tree(root);
+    free_tok_strings(token_arr);
     free(token_arr);
+    _CLOSE_LOG();
     return 0;
 }
