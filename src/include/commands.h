9d420f215c487e646e9b8614ea2626b733f6ea53
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-13 02:05:41 +0300

    команды переехали сюда

--- src/include/commands.h
+++ src/include/commands.h
@@ -1,7 +1,41 @@
 #ifndef COMMANDS
 #define COMMANDS
 
-#define COMMANDS_NUMBER 13
+#define COMMANDS_NUMBER 21
+
+enum COMMANDS
+{
+    PLUS    = 0x21,
+    MINUS   = 0x22,
+    STAR    = 0x23,
+    SLASH   = 0x24,
+    POW     = 0x25,
+
+    E       = 0x41,
+
+    OP_BR   = 0x01,
+    CL_BR   = 0x02,    
+    STR_END = 0x03,
+
+    BODY    = 0x0F,
+
+    LOG_E,
+    LOG_NE,
+    LOG_A,
+    LOG_AE,
+    LOG_B,
+    LOG_BE,
+    
+
+    RET,
+    PTR_VAL,
+
+
+    OP_F_BR,
+    CL_F_BR,
+    ZAP,
+    INCL,
+};
 
 typedef struct
 {
