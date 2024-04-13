fdf451ab1e611f3be93e4848981e87e40698db38
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-14 01:04:27 +0300

    добавлен вызов функции компилятора

--- src/main.cpp
+++ src/main.cpp
@@ -9,15 +9,23 @@ int main(int argc, char const *argv[])
     _OPEN_LOG("logs/main.log");
 
     token_t *token_arr = {0};
-    
+    LOG("-----------------------------TOKENIZATION---------------------\n");
     int error = tokenize(&token_arr, argv[argc - 1]);
     if (error)
     {
         _CLOSE_LOG();
         return error;
     }
-    
+    LOG("\n\n-------------------------------PARSING-------------------------\n");
     node_t *root = create_syntax_tree(token_arr);
+    if (!root)
+        return NULL_ROOT_ERR;
+
+    error = compiler(root);
+    if (error)
+        return error;
+
+    LOG("\n\n--------------------------------CREATING GRAPH------------------\n");
     create_gparh_code(root, EXPR);
 
     kill_tree(root);
