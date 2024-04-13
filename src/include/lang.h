a78655bb7db4d267a1aeb00546055c2acf28ba5d
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-14 01:06:00 +0300

    небольшие изменения

--- src/include/lang.h
+++ src/include/lang.h
@@ -3,20 +3,26 @@
 
 #include "../lang_parser/tokenizator.h"
 #include "../../graph_creator/create_graph.h"
+#include "../compiler/compiler.h"
 
 #ifdef DEBUG
 #define TOK_SHIFT()\
     tkns++;\
     LOG("> tokens shifted, current token is:\n");\
-    token_dump(logs, tkns, 1)
+    token_dump(logs, tkns, 1);
 #else
 #define TOK_SHIFT() tkns++
 #endif
 
 #define _ADD_B(arg1, arg2)\
-    if (add_branch(arg1, arg2))\
-        return NULL
+    do {if (add_branch(arg1, arg2) == NODE_MEM_ALC_ERR)\
+        return NULL;} while(0)
 
 node_t *create_syntax_tree(token_t *token_arr);    
+
+enum MAIN_ERR
+{
+    NULL_ROOT_ERR = 404,
+};
 
 #endif
