34d78885c1fd3343223f9644ee37b5a6a240733c
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-18 01:21:48 +0300

    кодогенерация выражений

---
+++ src/compiler/expr_func.cpp
@@ -0,0 +1,83 @@
+#include "../include/expr_func.h"
+
+int make_command(unsigned char cmd);
+
+int expr_in_asm(variables *var_arr, node_t *node)
+{
+    assert(asm_file);
+    assert(node);
+    assert(var_arr);
+    
+    LOG("node %p was given for calculation\n", node);
+
+    if (node->data_type == NUM)
+    {
+        LOG("> number %.2lf was found and will be pushed in stack\n", node->data.number);
+        fprintf(asm_file, "push %lf\n", node->data.number);
+        return 0;
+    }
+    if (node->data_type == VAR)
+    {
+        variable_t *var = find_var(var_arr, node->data.string);
+        if (!var)
+        {
+            LOG(">>> variable wasn't defined in expression%40s\n", "[error]");
+            return 1;
+        }
+
+        LOG("> variable %s was found on the address %d and will be pushed in stack\n", var->var, var->rel_address);
+        fprintf(asm_file, "push [rbp+%d]\n", var->rel_address);
+        return 0;
+    }
+    if (node->data_type == OP)
+    {
+        if (node->branch_number != 2)
+        {
+            LOG(">>> fatal error, arithmetic OP has more than 2 nodes%40s\n", "[error]");
+            return 1;
+        }
+        
+        if (expr_in_asm(var_arr, node->branches[L]))
+            return 1;
+        if (expr_in_asm(var_arr, node->branches[R]))
+            return 1;
+
+        if (make_command(node->data.command))
+            return 1;
+            
+        return 0;
+    }
+    
+
+    LOG(">>> fatal error: unacceptable data type of the node for the expression%40s\n", "[error]");
+    return 1;
+}
+
+int make_command(unsigned char cmd)
+{
+    switch (cmd)
+    {
+    case PLUS:
+        fprintf(asm_file, "add\n");
+        break;
+    case MINUS:
+        fprintf(asm_file, "sub\n");
+        break;
+    case STAR:
+        fprintf(asm_file, "mult\n");
+        break;
+    case SLASH:
+        fprintf(asm_file, "div\n");
+        break;
+    case POW:
+        fprintf(asm_file, "pow\n");
+        break;
+
+    default:
+        LOG(">>> fatal error occured: couldn't detect arithmetic operation: %d%40s\n", cmd, "[error]");
+        return 1;
+        break;
+    }
+
+    return 0;
+}
