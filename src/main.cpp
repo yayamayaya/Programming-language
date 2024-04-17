2ff5627373ff7d6bddc1ddbe687ce47b9931f78a
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-18 01:19:12 +0300

    мейн расширен

--- src/main.cpp
+++ src/main.cpp
@@ -9,28 +9,33 @@ int main(int argc, char const *argv[])
     _OPEN_LOG("logs/main.log");
 
     token_t *token_arr = {0};
-    LOG("-----------------------------TOKENIZATION---------------------\n");
+    LOG("-----------------------------------TOKENIZATION---------------------\n");
     int error = tokenize(&token_arr, argv[argc - 1]);
     if (error)
     {
         _CLOSE_LOG();
         return error;
     }
-    LOG("\n\n-------------------------------PARSING-------------------------\n");
+    LOG("\n\n-------------------------------PARSING--------------------------\n");
     node_t *root = create_syntax_tree(token_arr);
     if (!root)
+    {
+        free_tok_strings(token_arr);
+        free(token_arr);
+        _CLOSE_LOG();
         return NULL_ROOT_ERR;
-
-    error = compiler(root);
-    if (error)
-        return error;
+    }
 
     LOG("\n\n--------------------------------CREATING GRAPH------------------\n");
     create_gparh_code(root, EXPR);
+    
+    LOG("\n\n--------------------------------COMPILING-----------------------\n");
+    error = compiler(root);
 
     kill_tree(root);
     free_tok_strings(token_arr);
     free(token_arr);
+    LOG(">> programm ended\n");
     _CLOSE_LOG();
-    return 0;
+    return error;
 }
