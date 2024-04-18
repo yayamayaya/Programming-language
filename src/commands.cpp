bcaf787fb4cb4eec8c168e88e02f16a61f09c2b8
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-18 03:05:48 +0300

    Добавлены новые команды

--- src/commands.cpp
+++ src/commands.cpp
@@ -3,27 +3,33 @@
 command_t commands[COMMANDS_NUMBER] = 
 {
     {E, "=", 1},
-    {LOG_E, "==", 2},
+    {VAR_END, "brat", 4},
+
+    {PLUS, "+", 1},
+    {MINUS, "-", 1},
+    {STAR, "*", 1},
+    {SLASH, "/", 1},
+    {POW, "^", 1},
+    {LOG_E, "loge", 4},
     {LOG_NE, "!=", 2},
     {LOG_A, ">", 1},
     {LOG_AE, ">=", 2},
     {LOG_B, "<", 1},
     {LOG_BE, "<=", 2},
     {STR_END, "eu", 2},
+
     {OP_BR, "(", 1},
     {CL_BR, ")", 1},
     {OP_F_BR, "{", 1},
     {CL_F_BR, "}", 1},
+
+    {IF, "if", 2},
+    {WHILE, "while", 5},
+
     {RET, "sygeide", 7},
-    {PLUS, "+", 1},
-    {MINUS, "-", 1},
-    {STAR, "*", 1},
-    {SLASH, "/", 1},
-    {POW, "^", 1},
     {ZAP, ",", 1},
     {PTR_VAL, "Pavlodar,brat", 13},
     {INCL, "include", 7},
-    {IF, "if", 2},
 
 
 
--- src/include/commands.h
+++ src/include/commands.h
@@ -1,7 +1,7 @@
 #ifndef COMMANDS
 #define COMMANDS
 
-#define COMMANDS_NUMBER 22
+#define COMMANDS_NUMBER 24
 
 enum COMMANDS
 {
@@ -10,8 +10,15 @@ enum COMMANDS
     STAR    = 0x23,
     SLASH   = 0x24,
     POW     = 0x25,
+    LOG_E   = 0x26,
+    LOG_NE  = 0x27,
+    LOG_A   = 0x28,
+    LOG_AE  = 0x29,
+    LOG_B   = 0x2A,
+    LOG_BE  = 0x2B,    
 
     E       = 0x41,
+    VAR_END = 0x42,
 
     OP_BR   = 0x01,
     CL_BR   = 0x02,
@@ -20,16 +27,12 @@ enum COMMANDS
     STR_END = 0x07,
 
     IF      = 0x11,
+    WHILE   = 0x12,
 
     BODY    = 0x0F,
     EXPR    = 0x0E,
 
-    LOG_E,
-    LOG_NE,
-    LOG_A,
-    LOG_AE,
-    LOG_B,
-    LOG_BE,
+
     
 
     RET,
