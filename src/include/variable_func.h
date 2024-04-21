b94d7a95ddc9a5089890dce77ec12d837a6f9025
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-21 17:48:15 +0300

    новая функция добавления переменной в проц. стек

--- src/include/variable_func.h
+++ src/include/variable_func.h
@@ -4,7 +4,6 @@
 #include <string.h>
 #include "compiler_structs.h"
 #include "n_node.h"
-#include "../../../stack/src/stack.h"
 #include "commands.h"
 #include "expr_func.h"
 
@@ -13,6 +12,7 @@
 int assign_variable(Stack <variable_t> *vars, node_t *node);
 int create_variable(Stack <variable_t> *vars, node_t *node);
 variable_t *find_var(Stack <variable_t> *vars, const char *var_name);
+int push_var_in_asm(Stack <variable_t> *vars, const char *var_name);
 void var_dump(Stack <variable_t> *vars);
 
 enum VAR_ERRORS
