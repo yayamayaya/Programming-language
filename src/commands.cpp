7c05050ea686b857fbf23f238dc4f6cc5ffc8e0a
yayamayaya <semenov.aleksandr@phystech.edu>
2024-04-22 15:21:43 +0300

    имена команд переосмыслены

--- src/commands.cpp
+++ src/commands.cpp
@@ -2,9 +2,9 @@
 
 command_t commands[COMMANDS_NUMBER] = 
 {
-    {E, "=", 1},
+    {E, "bolad", 5},
     {VAR_END, "brat", 4},
-    {ZAP, ",", 1},
+    {ZAP, "men", 3},
 
     {PLUS, "+", 1},
     {MINUS, "-", 1},
@@ -17,16 +17,16 @@ command_t commands[COMMANDS_NUMBER] =
     {LOG_AE, ">=", 2},
     {LOG_B, "<", 1},
     {LOG_BE, "<=", 2},
-    {STR_END, "eu", 2},
+    {STR_END, "eu,", 3},
 
-    {OP_BR, "(", 1},
-    {CL_BR, ")", 1},
-    {OP_F_BR, "{", 1},
-    {CL_F_BR, "}", 1},
+    {OP_BR, "pavlodar", 8},
+    {CL_BR, "kalasy", 6},
+    {OP_F_BR, "kraba", 5},
+    {CL_F_BR, "nakin'", 6},
 
-    {IF, "if", 2},
-    {WHILE, "while", 5},
+    {IF, "eitkezinba", 11},
+    {WHILE, "shenber", 7},
     {RET, "sygeide", 7},
-    {PRINT, "print", 5},
+    {PRINT, "korsetsh", 8},
 };
     //{INCL, "include", 7},
