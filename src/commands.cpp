e6ac6574083c4a49b9ccd462076bd827777a680f
yaya <semenov.aleksandr@phystech.edu>
2024-04-26 23:49:28 +0300

    добавлен упрощённый набор команд, для облегчения дебага

--- src/commands.cpp
+++ src/commands.cpp
@@ -1,5 +1,39 @@
 #include "include/commands.h"
 
+
+#ifdef SIMPL
+
+command_t commands[COMMANDS_NUMBER] = 
+{
+    {E, "=", 1},
+    {VAR_END, "end", 3},
+    {ZAP, ",", 1},
+
+    {PLUS, "+", 1},
+    {MINUS, "-", 1},
+    {STAR, "*", 1},
+    {SLASH, "/", 1},
+    {POW, "^", 1},
+    {LOG_E, "loge", 4},
+    {LOG_NE, "!=", 2},
+    {LOG_A, ">", 1},
+    {LOG_AE, ">=", 2},
+    {LOG_B, "<", 1},
+    {LOG_BE, "<=", 2},
+    {STR_END, ";", 1},
+
+    {OP_BR, "(", 1},
+    {CL_BR, ")", 1},
+    {OP_F_BR, "{", 1},
+    {CL_F_BR, "}", 1},
+
+    {IF, "if", 2},
+    {WHILE, "while", 5},
+    {RET, "return", 6},
+    {PRINT, "print", 5},
+};
+
+#else
 command_t commands[COMMANDS_NUMBER] = 
 {
     {E, "bolad", 5},
@@ -24,9 +58,10 @@ command_t commands[COMMANDS_NUMBER] =
     {OP_F_BR, "kraba", 5},
     {CL_F_BR, "nakin'", 6},
 
-    {IF, "eitkezinba", 11},
+    {IF, "eitkezinba", 10},
     {WHILE, "shenber", 7},
     {RET, "sygeide", 7},
     {PRINT, "korsetsh", 8},
 };
     //{INCL, "include", 7},
+#endif
