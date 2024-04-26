5d94b6ff6c6d2787b053b9779609b8dd5a0d8049
yaya <semenov.aleksandr@phystech.edu>
2024-04-27 02:39:59 +0300

    Сделал dsl, поправил кодстайл

--- src/translator/trans_expr.cpp
+++ src/translator/trans_expr.cpp
@@ -1,8 +1,6 @@
-#include "../include/expr_func.h"
+#include "../include/trans_expr.h"
 
 int make_command(unsigned char cmd);
-
-//TO DO сделать dsl, проверка деления на 0
 
 int expr_in_asm(Stack <variable_t> *vars, node_t *node)
 {
@@ -11,40 +9,37 @@ int expr_in_asm(Stack <variable_t> *vars, node_t *node)
     assert(vars);
     
     LOG("node %p was given for calculation\n", node);
-
     int error = 0;
+
     switch (node->data_type)
     {
     case NUM:
         LOG("> number %.2lf was found and will be pushed in stack\n", node->data.number);
-        fprintf(asm_file, "push %lf\n", node->data.number);
+        _PUSH_NUM(node->data.number);
         return 0;
 
     case OP:
-        if (node->branch_number != 2)
-        {
-            LOG(">>> fatal error, arithmetic OP has more than 2 nodes%40s\n", "[error]");
-            return 1;
-        }
+        _CHECK_NODE_NUM(2);
         
         if (expr_in_asm(vars, node->branches[L]))
-            return 1;
+            return FATAL_ERR;
         if (expr_in_asm(vars, node->branches[R]))
-            return 1;
+            return FATAL_ERR;
 
         if (make_command(node->data.command))
-            return 1;
+            return FATAL_ERR;
             
         return 0;
     
     case VAR:
         error = push_var_in_asm(vars, node->data.string);
         return 0;
+        
     case FUNC:
         LOG("> function call in expression was found\n");
         error = call_func(vars, node);
 
-        fprintf(asm_file, "push dx\n");
+        //_PUSH_REG("dx");
         return error;
     case CONN:
         if (node->data.command == EXPR)
@@ -55,51 +50,28 @@ int expr_in_asm(Stack <variable_t> *vars, node_t *node)
     }
 
     LOG(">>> fatal error: unacceptable data type of the node for the expression%40s\n", "[error]");
-    return 1;
+    return FATAL_ERR;
 }
 
 int make_command(unsigned char cmd)
 {
     switch (cmd)
     {
-    case PLUS:
-        fprintf(asm_file, "add\n");
-        break;
-    case MINUS:
-        fprintf(asm_file, "sub\n");
-        break;
-    case STAR:
-        fprintf(asm_file, "mult\n");
-        break;
-    case SLASH:
-        fprintf(asm_file, "div\n");
-        break;
-    case POW:
-        fprintf(asm_file, "pow\n");
-        break;
-    case LOG_E:
-        fprintf(asm_file, "log_e\n");
-        break;
-    case LOG_NE:
-        fprintf(asm_file, "log_ne\n");
-        break;
-    case LOG_A:
-        fprintf(asm_file, "log_a\n");
-        break;
-    case LOG_AE:
-        fprintf(asm_file, "log_ae\n");
-        break;
-    case LOG_B:
-        fprintf(asm_file, "log_b\n");
-        break;
-    case LOG_BE:
-        fprintf(asm_file, "log_be\n");
-        break;
+    case PLUS:      _TRANSL_OP("add");
+    case MINUS:     _TRANSL_OP("sub");
+    case STAR:      _TRANSL_OP("mult");
+    case SLASH:     _TRANSL_OP("div");
+    case POW:       _TRANSL_OP("pow");
+    case LOG_E:     _TRANSL_OP("log_e");
+    case LOG_NE:    _TRANSL_OP("log_ne");
+    case LOG_A:     _TRANSL_OP("log_a");
+    case LOG_AE:    _TRANSL_OP("log_ae");
+    case LOG_B:     _TRANSL_OP("log_b");
+    case LOG_BE:    _TRANSL_OP("log_be");
 
     default:
         LOG(">>> fatal error occured: couldn't detect arithmetic operation: %d%40s\n", cmd, "[error]");
-        return 1;
-        break;
+        return FATAL_ERR;
     }
 
     return 0;
